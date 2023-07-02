5af282e20c4db7ff7934232f21400e8a65a57f6c
victormwachi <mudakizevictor@gmail.com>
2023-07-02 12:08:42 +0300

    pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,6 +1,19 @@
+#!/usr/bin/python3
 def pascal_triangle(n):
-  list = []
-  if n > 0:
-    for i in range(1,n+1):
-      
-    
+    triangle = []
+    if n <= 0:
+        return triangle
+    for i in range(1, n + 1):
+        if i == 1:
+            triangle.append([1])
+            triangle.append([1,1])
+        else:
+            temp = []
+            idx = 0
+            temp.append(1)
+            while idx + 1 <= len(triangle[-1]) - 1:
+                temp.append(triangle[-1][idx] + triangle[-1][idx + 1])
+                idx += 1
+            temp.append(1)
+            triangle.append(temp)
+    return triangle
