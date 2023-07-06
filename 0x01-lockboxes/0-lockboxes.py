d2c4abdd1ff87bca81b50aeeb115dfafea6400dd
victormwachi <mudakizevictor@gmail.com>
2023-07-06 23:44:53 +0300

    locable boxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,2 +1,12 @@
 def canUnlockAll(boxes):
-  return false
+    l = len(boxes)
+    seen_boxes = set([0])
+    unseen_boxes = set(boxes[0]).difference(set([0]))
+    while len(unseen_boxes) > 0:
+        box_index = unseen_boxes.pop()
+        if not box_index or box_index >= l or box_index < 0:
+            continue
+        if box_index not in seen_boxes:
+            unseen_boxes = unseen_boxes.union(boxes[box_index])
+            seen_boxes.add(box_index)
+    return l == len(seen_boxes)
