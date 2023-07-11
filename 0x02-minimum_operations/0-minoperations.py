a79fad79b72c589d43319a580b6a0984a32158c2
victormwachi <mudakizevictor@gmail.com>
2023-07-12 00:00:45 +0300

    0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python3
 """
-0. Minimum Operations 
+0. Minimum Operations
 """
 
 
 def minOperations(n):
-  """
-  calculates the fewest number of operations needed
-  to result in exactly n H characters in the file
-  """
-  #
-  if num > n:
-    return 0
+    """
+    calculates the fewest number of operations needed
+    to result in exactly n H characters in the file
+    """
+    if n < 2:
+        return 0
+    while n % 2 == 0:
+        prime_list.append(2)
+        n /= 2
+        for i in range(3, int(Math.sqrt(n)) + 1, 2):
+            while n % i == 0:
+                prime_list.append(i)
+                n /= i
+        if n > 2:
+            prime_list.append(i)
+    return sum(prime_list)
