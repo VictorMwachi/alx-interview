45d19e2e336492831072f39f68e2f40218f47707
Victor Mudaki Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2023-07-11 16:38:28 +0300

    Create 0-minoperations.py

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+0. Minimum Operations 
+"""
+
+
+def minOperations(n):
+  """
+  calculates the fewest number of operations needed
+  to result in exactly n H characters in the file
+  """
+  #
+  if num > n:
+    return 0
