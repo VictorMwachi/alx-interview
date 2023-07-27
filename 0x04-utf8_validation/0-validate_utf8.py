adb45794b4ef83e6aa10ac9b2173a461333733b0
victormwachi <mudakizevictor@gmail.com>
2023-07-27 22:43:33 +0300

    0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""0. UTF-8 Validation """
+
+
+def validUTF8(data):
+    """determines if a given data set represents a valid UTF-8 encoding."""
+   if False in [num <= 127 for num in data]:
+       return False
+   else:
+       return True
