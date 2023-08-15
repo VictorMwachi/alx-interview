c598bd5265fb19f7d8467b1f658d6101f5a9b1ef
Victor Mwachi <mudakizevictor@gmail.com>
2023-08-15 08:21:41 +0000

    rotate

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -3,14 +3,15 @@
 Rotate 2D Matrix
 """
 
+
 def rotate_2d_matrix(matrix):
-  """rotates the 2d matrix clockwise 90deg"""
-  mat =[]
-  col = 0
-  while col < len(matrix):
-      rotated_row =[]
-      for row in matrix:
-          rotated_row.append(row[col])
-          mat.append(rotated_row)
-    col += 1
+    """rotates the 2d matrix clockwise 90deg"""
+    mat = []
+    col = 0
+    while col < len(matrix):
+        rotated_row = []
+        for row in matrix:
+            rotated_row.append(row[col])
+        mat.append(rotated_row)
+        col += 1
     matrix = mat
